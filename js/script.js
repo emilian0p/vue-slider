61405bc1b02a034bcd4a8fef1a745e771dee5081
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-05 18:36:34 +0200

    errore di sintassi per una parentesi che sembra chiusa, non so come sistemare, forse sbagliata sintassi?

--- js/script.js
+++ js/script.js
@@ -30,46 +30,58 @@
 
 /* custom js */
 
-const app  = new Vue (
-    {
-        el : '#app' 
+const app = new Vue ( {
+    
+    el : '#app'
 
-        data : {
-
-            i = 0,
-             
-            slides = [
-                {
-                    image: 'img/01.jpg',
-                    title: 'Svezia',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                },
-                {
-                    image: 'img/02.jpg',
-                    title: 'Svizzera',
-                    text: 'Lorem ipsum.',
-                },
-                {
-                    image: 'img/03.jpg',
-                    title: 'Gran Bretagna',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                },
-                {
-                    image: 'img/04.jpg',
-                    title: 'Germania',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-                },
-                {
-                    image: 'img/05.jpg',
-                    title: 'Paradise',
-                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-                }
-            ],
-        }
+    data : {
+        index = 0,
+        slides : [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ]
+    },
 
 
-        methods : {
+methods : {
 
-        }
+    next : function() {
+        this.index++ ;
+        if ( this.index === this.slides.lenght ) {
+            this.index = 0;
+        },
+
+        prev : function() {
+            this.index-- ;
+            if ( this.index=== -1 ) {
+                this.index = this.slides.lenght -1 ;
+            }
+        },
     }
+}
+}
+
 )
