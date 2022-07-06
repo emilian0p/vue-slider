5397bb14a7fa24c3ab31347ea7fa05b28c159c26
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-06 11:28:11 +0200

    non va

--- js/script.js
+++ js/script.js
@@ -30,58 +30,65 @@
 
 /* custom js */
 
-const app = new Vue ( {
+const app = new Vue(
+    {
+        el: '#app'
     
-    el : '#app'
+        data: {
+            
+            activeImageIndex: 0,
 
-    data : {
-        index = 0,
-        slides : [
-            {
-                image: 'img/01.jpg',
-                title: 'Svezia',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-            },
-            {
-                image: 'img/02.jpg',
-                title: 'Svizzera',
-                text: 'Lorem ipsum.',
-            },
-            {
-                image: 'img/03.jpg',
-                title: 'Gran Bretagna',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-            },
-            {
-                image: 'img/04.jpg',
-                title: 'Germania',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-            },
-            {
-                image: 'img/05.jpg',
-                title: 'Paradise',
-                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-            }
-        ]
-    },
-
-
-methods : {
-
-    next : function() {
-        this.index++ ;
-        if ( this.index === this.slides.lenght ) {
-            this.index = 0;
+            slides: [
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum.',
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    image: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                }
+            ]
         },
 
-        prev : function() {
-            this.index-- ;
-            if ( this.index=== -1 ) {
-                this.index = this.slides.lenght -1 ;
-            }
-        },
-    }
-}
-}
 
+        methods: {
+
+            nextPicture: function () {
+
+                if (this.activeImageIndex == this, slides.length - 1) {
+                    this.activeImageIndex = 0;
+                } else {
+                    this.activeImageIndex++;
+                }
+            },
+
+            prevPicture: function () {
+                if (this.activeImageIndex === 0) {
+                    this.activeImageIndex = this.slides.length - 1;
+                } else {
+                    this.activeImageIndex--;
+                }
+            },
+
+
+        }
+
+            
 )
